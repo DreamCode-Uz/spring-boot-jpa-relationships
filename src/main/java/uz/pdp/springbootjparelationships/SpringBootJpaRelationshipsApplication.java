a476f5726081377ba6819c8938b23ae863664b73
DreamCode-Uz <javadeveloperbackend999@gmail.com>
2022-07-28 20:01:07 +0300

    created package and entity

---
+++ src/main/java/uz/pdp/springbootjparelationships/SpringBootJpaRelationshipsApplication.java
@@ -0,0 +1,13 @@
+package uz.pdp.springbootjparelationships;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJpaRelationshipsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJpaRelationshipsApplication.class, args);
+    }
+
+}
---
+++ src/main/java/uz/pdp/springbootjparelationships/entity/University.java
@@ -0,0 +1,4 @@
+package uz.pdp.springbootjparelationships.entity;
+
+public class University {
+}
