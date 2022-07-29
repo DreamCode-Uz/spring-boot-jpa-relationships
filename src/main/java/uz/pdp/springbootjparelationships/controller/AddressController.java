6f3ea8e8d78e8a38bc556f2a8b10e0915aef88e2
DreamCode-Uz <javadeveloperbackend999@gmail.com>
2022-07-29 10:28:38 +0300

    update

--- src/main/java/uz/pdp/springbootjparelationships/controller/AddressController.java
+++ src/main/java/uz/pdp/springbootjparelationships/controller/AddressController.java
@@ -1,7 +1,9 @@
 package uz.pdp.springbootjparelationships.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/address")
 public class AddressController {
 }
--- src/main/java/uz/pdp/springbootjparelationships/service/AddressService.java
+++ src/main/java/uz/pdp/springbootjparelationships/service/AddressService.java
@@ -1,7 +1,25 @@
 package uz.pdp.springbootjparelationships.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import uz.pdp.springbootjparelationships.entity.Address;
+import uz.pdp.springbootjparelationships.repository.AddressRepository;
+
+import java.util.List;
 
 @Service
 public class AddressService {
+
+    private final AddressRepository repository;
+
+    @Autowired
+    public AddressService(AddressRepository repository) {
+        this.repository = repository;
+    }
+
+    //    READ
+//    public ResponseEntity<List<Address>> getAddress() {
+//
+//    }
 }
