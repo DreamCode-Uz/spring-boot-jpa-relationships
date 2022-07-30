e67322011db87732c8cf8247b1904a1cd01e79a6
DreamCode-Uz <javadeveloperbackend999@gmail.com>
2022-07-30 08:29:41 +0300

    Update app

--- src/main/java/uz/pdp/springbootjparelationships/client/StartedData.java
+++ src/main/java/uz/pdp/springbootjparelationships/client/StartedData.java
@@ -4,20 +4,39 @@ import uz.pdp.springbootjparelationships.payload.FacultyDTO;
 import uz.pdp.springbootjparelationships.payload.UniversityDTO;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class StartedData {
-    public static final String[] subjects = {"Matematika", "Kimyo", "English tili", "Rus Tili", "Ona Tili", "Adabiyot", "Tarix", "Biologiya"};
+    public static final String[] subjects = {"Matematika", "Fizika", "Kimyo", "Organik kimyo", "Anorganik kimyo", "Neft-gaz kimyosi", "English tili", "Rus Tili", "Ona Tili", "Adabiyot", "Tarix", "Biologiya", "Zoologiya", "Biofizika", "Ekologiya"};
 
     public static List<UniversityDTO> universities() {
         List<UniversityDTO> universities = new ArrayList<>();
-        universities.add(new UniversityDTO("O'zbekiston", "Tashkent", "Olmazor", "Usmon Nosir"));
-        universities.add(new UniversityDTO("Tibbiyot", "Tashkent", "Beruniy", "M.Ulug'bek"));
-        universities.add(new UniversityDTO("Pedagogika", "Tashkent", "Beruniy", "A.Navoiy"));
+        universities.add(new UniversityDTO("O'zbekiston milliy universiteti", "Tashkent shahar", "Talabalar shaharchasi", "Beruniy Metrosi"));
+        universities.add(new UniversityDTO("Toshkent davlat texnika universiteti", "Tashkent shahar", "Talabarlar shaharchasi", "Universitet ko'chasi"));
+        universities.add(new UniversityDTO("Jahon tillari universiteti", "Tashkent", "Uchtepa tumani", "Кичик халқа йўли 21 а"));
         universities.add(new UniversityDTO("Turin Palitexnika", "Tashkent", "Olmazor", "Amir Temur"));
-        universities.add(new UniversityDTO("Jahon Tillari", "Tashkent", "Olmazor", "Amir temur"));
         return universities;
     }
 
-//    public static List<FacultyDTO> facultys
+    public static List<FacultyDTO> faculties() {
+        List<FacultyDTO> faculties = new ArrayList<>(Arrays.asList(
+                new FacultyDTO("Kimyo", 1),
+                new FacultyDTO("Matematika", 1),
+                new FacultyDTO("Biologiya", 1),
+                new FacultyDTO("Xorijiy filologiya", 1),
+                new FacultyDTO("Ijtimoiy fanlar", 1),
+                new FacultyDTO("Tarix", 1),
+                new FacultyDTO("Ekologiya", 1),
+                new FacultyDTO("Iqtisodiyot", 1),
+                new FacultyDTO("Ingliz tili", 3),
+                new FacultyDTO("Rus filologiyasi", 3),
+                new FacultyDTO("Xalqaro Jurnalistika", 3),
+                new FacultyDTO("Naft va Gaz", 2),
+                new FacultyDTO("Muxandislik", 2),
+                new FacultyDTO("Issiqlik eneregetika", 2),
+                new FacultyDTO("Elektr energetikasi", 1)
+        ));
+        return faculties;
+    }
 }
--- src/main/java/uz/pdp/springbootjparelationships/payload/FacultyDTO.java
+++ src/main/java/uz/pdp/springbootjparelationships/payload/FacultyDTO.java
@@ -1,7 +1,9 @@
 package uz.pdp.springbootjparelationships.payload;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
+@AllArgsConstructor
 @Data
 public class FacultyDTO {
     private String name;
